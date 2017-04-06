8b0335123a86cc35f61e905c42f04c2dd857cb33
Elisa <Elisa@A230474-PC>
2017-04-06 15:00:09 +0200

    prova elisa

--- Risiko/src/risiko/Risiko.java
+++ Risiko/src/risiko/Risiko.java
@@ -15,9 +15,9 @@ public class Risiko {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ciao");
+        
         System.out.println("ciaone");
-        System.out.println("ciao");
+        
         
          
     }
